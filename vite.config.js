2747d95ddfffd93c44489b021b4b793215c3f391
clecio <chmm@cin.ufpe.br>
2026-08-16 17:57:50 -0300

    ajsute da pasta vite para rodar o MPA corretamente

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
+  build: {
+    rolldownOptions: {
+      input: {
+        main: resolve(import.meta.dirname, 'index.html'),
+        portfolio: resolve(import.meta.dirname, 'portfolio/index.html'),
+      },
+    },
+  },
 })
